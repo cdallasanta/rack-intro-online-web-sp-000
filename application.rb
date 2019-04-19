8ed7cfb72ff8f28a09fee3ae6b92444c37e227d1
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-19 17:31:41 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World. Welcome to my pants."
+    resp.write "Hello, my name is Chris."
     resp.finish
   end
 
