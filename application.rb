435c1f8ddc17056a37ff8076019932a2998af2a4
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-19 17:30:55 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World. Welcome to my pants."
     resp.finish
   end
 
 end
-
